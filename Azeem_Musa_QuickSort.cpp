192d2e1f4c18c8594e98f9682ad3667c22b6c2f8
MMAzeem3 <musa.mazeem@gmail.com>
2022-11-17 17:46:48 -0500

    rand

--- Azeem_Musa_QuickSort.cpp
+++ Azeem_Musa_QuickSort.cpp
@@ -20,11 +20,16 @@
 #include <vector>
 #include <sstream>
 #include <exception>
+#include <cstdlib>
+#include <cmath>
 
 int read_file(const char *filename, std::vector<double> &A);
 int quick_sort(std::vector<double> & A);
+int quick_sort(std::vector<double> & A, const int l, const int r);
+int hoarse_partition(std::vector<double> &A, const int l, const int r);
+int generate_random_int(int lower, int upper);
 int write_file(const char *filename, const std::vector<double> A);
-int print_array(const std::vector<double> A);
+void print_array(const std::vector<double> A);
 
 // Entry Point - Driver Code to run QuickSort on input array
 int main(int argc, char **argv) {
@@ -34,6 +39,8 @@ int main(int argc, char **argv) {
         std::cout << "Usage: ./quicksort [input file] [output file]" << std::endl;
         return 1;
     }
+
+    std::srand(time(0));        // Used for random pivot generation
     
     std::vector<double> A;      // Vector to represent input values
     
@@ -43,6 +50,9 @@ int main(int argc, char **argv) {
         write_file(argv[2], A);
         return 1;
     }
+
+    // Sort the array
+    quick_sort(A);
 
     write_file(argv[2], A);
 
@@ -70,7 +80,6 @@ int read_file(const char *filename, std::vector<double> &A){
         while(std::getline(in_file, line)) { 
             std::istringstream iss(line);   // Delim file line by whitespace
             while (iss >> value) {
-                std::cout << "value: " << value << std::endl;
                 A.push_back(value);         // add each value to vector
             }
         }
@@ -118,35 +127,113 @@ int write_file(const char *filename, const std::vector<double> A) {
 
 int quick_sort(std::vector<double> & A) {
     /**
+     * Implements the QuickSort algorithm, as shown in the psuedo code
+     * Algorithm is implemented in overloaded recursive function
+     * 
+     * Paramters:
+     *      A (vector<double>)  :   Array to sort
+     * 
+     * Returns:
+     *      (int)   :   returns 1 to indicate success
+     * 
+     * Psuedo code:
+     * 
      *  ALGORITHM Quicksort(A[l..r])
      *      // Input: Subarray of array A[0..n-1], defined by its left and
      *      //        right indices l and r
      *      // Output: Subarray A[l..r] sorted in nondecreasing order
      *      
      *      if l < r
-     *          s <- LomutoPartition(A[l..r])     // s is a split position
+     *          s <- HoarsePartition(A[l..r])     // s is a split position
      *          Quicksort(A[l..s-1])
      *          Quicksort(A[s+1..r])
      * 
-     *  ALGORITHM LomutoPartition(A[l..r])
-     *      // Partitions subarray by Lomuto’s algorithm using first element as 
-     *         pivot
-     *      // Input: A subarray A[l..r] of array A[0..n − 1], defined by its 
-     *               left and right // indices l and r (l ≤ r)
-     *      // Output: Partition of A[l..r] and the new position of the pivot
-     * 
-     *      p <- A[l]
-     *      s <- l
-     *      for i <- l+1 to r do
-     *          if A[i] < p
-     *              s <- s+1
-     *              swap(A[s], A[i])
-     *      swap(A[l], A[s])
-     *      return s
+     *  ALGORITHM HoarsePartition(A[l..r])
+     *      // Partitions a subarray by Hoare’s algorithm, using a random pivot
+     *      // Input: Subarray of array A[0..n − 1], defined by its left and right
+     *      //        indices l and r (l < r)
+     *      // Output: Partition of A[l..r], with the split position returned as
+     *      //         this function’s value
+     *      p <- A[random(l, r)]
+     *      i <- l
+     *      j <- r + 1
+     *      repeat
+     *          repeat i <- i + 1 until A[i] >= p 
+     *          repeat j <- j − 1 until A[j ] <= p 
+     *          swap(A[i], A[j])
+     *      until i >= j
+     *      swap(A[i], A[j]) //undo last swap when i >= j 
+     *      swap(A[l], A[j])
+     *      return j
      */
+
+    // QuickSort starting with first and last indices
+    return quick_sort(A, 0, A.size()-1);     
 }
 
-int print_array(const std::vector<double> A){
+int quick_sort(std::vector<double> &A, const int l, const int r) {
+    /**
+     * Implements the QuickSort algorithm to sort a given array
+     * 
+     * Parameters:
+     *  A (vector<double>)  :   subarray to sort
+     *  l (int) :   Index to start subarray
+     *  r (int) :   Index to stop subarray
+     * 
+     * Returns:
+     *  (int)   : returns 1 to indicate success
+     */
+
+    hoarse_partition(A, l, r);
+
+    return 1;
+}
+
+int hoarse_partition(std::vector<double> &A, const int l, const int r) {
+    /**
+     * Implements a hoarse partition on a provided subarray
+     * Input:
+     *      A (vector<double>)  :   array to partition
+     *      l (int) :   Index to start subarray
+     *      r (int) :   Index to end subarray
+     * 
+     * Returns:
+     *  (int)   :   Index that split occurs
+     */
+
+    int s = 0;
+    int p = generate_random_int(l, r+1);
+    return s;
+}
+
+int generate_random_int(int lower, int upper) {
+    /**
+     * Generates a random integer in the range [lower, upper)
+     * 
+     * Parameters:
+     *  lower (int) :   lower limit of random number to generate (inclusive)
+     *  upper (int) :   upper limit of random number to generate (exclusive)
+     * 
+     * Returns:
+     *  (int)   :   Random integer within given range
+     */
+    
+    if (lower > upper) {
+        // Return lower limit and log error if range is invalid
+        std::cerr << "generate_random_int : invalid range" 
+            << " - returning lower limit" << std::endl;
+        return lower;
+    }
+    if (lower == upper) {
+        // Return lower limit if the range is 0
+        return lower;
+    }
+    // Return a random integer within given range
+    int range = upper - lower;
+    return std::floor(std::rand() % range + lower);
+}
+
+void print_array(const std::vector<double> A){
     for (auto num : A){
         std::cout << num  << " ";
     }
