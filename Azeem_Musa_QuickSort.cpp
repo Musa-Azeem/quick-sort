cd1b8354120640494ea4ea217924f15a564c1d15
MMAzeem3 <musa.mazeem@gmail.com>
2022-11-17 19:54:00 -0500

    organize

--- Azeem_Musa_QuickSort.cpp
+++ Azeem_Musa_QuickSort.cpp
@@ -23,6 +23,7 @@
 #include <cstdlib>
 #include <cmath>
 
+// Function Headers
 int read_file(const char *filename, std::vector<double> &A);
 int quick_sort(std::vector<double> & A);
 int quick_sort(std::vector<double> & A, const int l, const int r);
@@ -61,75 +62,10 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-int read_file(const char *filename, std::vector<double> &A){
-    /**
-     * This function reads a file from a given filename and populates the
-     *  given array with the values
-     * Input file
-     *  - File containing floating point numbers seperated by whitespace
-     * 
-     * Parameters:
-     *      filename (char *)   :   name of file to read values from
-     *      A (vector<double>)  :   Output vector with values read from file
-     */
-
-    std::ifstream in_file(filename);    // Input file stream
-    std::string line;                   // line of input file
-    double value;                       // each value read from file
-
-    if (in_file) {
-        // Get each line of input file 
-        while(std::getline(in_file, line)) { 
-            std::istringstream iss(line);   // Delim file line by whitespace
-            while (iss >> value) {
-                A.push_back(value);         // add each value to vector
-            }
-        }
-    }
-    else {
-        std::cerr << "Error Opening Input File" << std::endl;
-        return 0;   // return 0 to indicate failure
-    }
-
-    // Check if file was empty or did not exist
-    if (A.size() == 0) {
-        std::cerr << "Input file is empty or does not exist - sorting not complete" << std::endl;
-        return 0;   // return 0 to indicate failure
-    }
-    
-    return 1;
-}
-
-int write_file(const char *filename, const std::vector<double> A) {
-    /**
-     * This function writes a vector to a file of a given name
-     * Output file
-     *  - Outputs values of array seperated by whitespace
-     * 
-     * Parameters:
-     *      filename (char *)   :   Name of file to write values to
-     *      A (vector<double>)  :   Vector with values to write to file
-     */
-
-    std::ofstream out_file(filename);
-
-    if (out_file) {
-        for (auto val : A) {
-            out_file << val << " ";
-        }
-    }    
-    else {
-        std::cerr << "Error Opening Output File" << std::endl;
-        return 0;   // return 0 to indicate failure
-    }
-
-    return 1;
-}
-
-
 int quick_sort(std::vector<double> & A) {
     /**
-     * Implements the QuickSort algorithm, as shown in the psuedo code
+     * Implements the QuickSort algorithm using  the 
+     * Hoarse Partition Algorithm with random pivots
      * Algorithm is implemented in overloaded recursive function
      * 
      * Paramters:
@@ -138,35 +74,6 @@ int quick_sort(std::vector<double> & A) {
      * Returns:
      *      (int)   :   returns 1 to indicate success
      * 
-     * Psuedo code:
-     * 
-     *  ALGORITHM Quicksort(A[l..r])
-     *      // Input: Subarray of array A[0..n-1], defined by its left and
-     *      //        right indices l and r
-     *      // Output: Subarray A[l..r] sorted in nondecreasing order
-     *      
-     *      if l < r
-     *          s <- HoarsePartition(A[l..r])     // s is a split position
-     *          Quicksort(A[l..s-1])
-     *          Quicksort(A[s+1..r])
-     * 
-     *  ALGORITHM HoarsePartition(A[l..r])
-     *      // Partitions a subarray by Hoare’s algorithm, using a random pivot
-     *      // Input: Subarray of array A[0..n − 1], defined by its left and right
-     *      //        indices l and r (l < r)
-     *      // Output: Partition of A[l..r], with the split position returned as
-     *      //         this function’s value
-     *      p <- A[random(l, r)]
-     *      i <- l
-     *      j <- r + 1
-     *      repeat
-     *          repeat i <- i + 1 until A[i] >= p 
-     *          repeat j <- j − 1 until A[j] <= p 
-     *          swap(A[i], A[j])
-     *      until i >= j
-     *      swap(A[i], A[j]) //undo last swap when i >= j 
-     *      swap(A[l], A[j])
-     *      return j
      */
 
     if (A.size() == 0 || A.size() == 1) {
@@ -295,6 +202,70 @@ void swap(std::vector<double> &A, const int i, const int j) {
     A[j] = tmp;
 }
 
+int read_file(const char *filename, std::vector<double> &A){
+    /**
+     * This function reads a file from a given filename and populates the
+     *  given array with the values
+     * Input file
+     *  - File containing floating point numbers seperated by whitespace
+     * 
+     * Parameters:
+     *      filename (char *)   :   name of file to read values from
+     *      A (vector<double>)  :   Output vector with values read from file
+     */
+
+    std::ifstream in_file(filename);    // Input file stream
+    std::string line;                   // line of input file
+    double value;                       // each value read from file
+
+    if (in_file) {
+        // Get each line of input file 
+        while(std::getline(in_file, line)) { 
+            std::istringstream iss(line);   // Delim file line by whitespace
+            while (iss >> value) {
+                A.push_back(value);         // add each value to vector
+            }
+        }
+    }
+    else {
+        std::cerr << "Error Opening Input File" << std::endl;
+        return 0;   // return 0 to indicate failure
+    }
+
+    // Check if file was empty or did not exist
+    if (A.size() == 0) {
+        std::cerr << "Input file is empty or does not exist - sorting not complete" << std::endl;
+        return 0;   // return 0 to indicate failure
+    }
+    
+    return 1;
+}
+
+int write_file(const char *filename, const std::vector<double> A) {
+    /**
+     * This function writes a vector to a file of a given name
+     * Output file
+     *  - Outputs values of array seperated by whitespace
+     * 
+     * Parameters:
+     *      filename (char *)   :   Name of file to write values to
+     *      A (vector<double>)  :   Vector with values to write to file
+     */
+
+    std::ofstream out_file(filename);
+
+    if (out_file) {
+        for (auto val : A) {
+            out_file << val << " ";
+        }
+    }    
+    else {
+        std::cerr << "Error Opening Output File" << std::endl;
+        return 0;   // return 0 to indicate failure
+    }
+
+    return 1;
+}
 
 void print_array(const std::vector<double> A){
     for (auto num : A){
